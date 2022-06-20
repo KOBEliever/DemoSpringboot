a4d3ce0e76f009bdb86344f8d1bb4cad83b24231
一 丁 <970102437@qq.com>
2022-06-20 08:27:40 +0800

    yaml数据读取

--- Springboot_hello/src/main/java/com/cqu/controller/BookController.java
+++ Springboot_hello/src/main/java/com/cqu/controller/BookController.java
@@ -1,5 +1,8 @@
 package com.cqu.controller;
 
+import com.cqu.domain.Enterprise;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/books")
 public class BookController {
+    @Value("${lesson}")
+    private String lesson;
+    @Value("${enterprise.subject[0]}")
+    private String subject;
+    @Autowired
+    private Enterprise enterprise;
+
     @GetMapping("/{id}")
     public String getById(@PathVariable Integer id){
-        System.out.println("id=>"+id);
+        System.out.println(lesson);
+        System.out.println(subject);
+        System.out.println(enterprise);
         return "hello,springboot!";
     }
 }
